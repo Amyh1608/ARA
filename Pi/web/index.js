4146c058162367848f69a8aceb2f7c975f667dab
Simon Fong <simonfong6@gmail.com>
2018-02-15 12:47:01 -0800

    Added image fetching to js

--- Pi/web/index.js
+++ Pi/web/index.js
@@ -1,9 +1,8 @@
-$('.lw').text("Hello");// Write JavaScript here 
 function servo(joint, degrees){
   console.log(joint,degrees);
   var joint_id = '#' + joint;
   $(joint_id).text(degrees);
-  var servo_url = "http://192.168.1.125:5000/servo/";
+  var servo_url = "http://localhost:5000/servo/";
   var servo_command = servo_url + joint +'/' + degrees;
   $.get(servo_command, function(data){
     console.log(data);
@@ -21,4 +20,11 @@ function servo_minus(joint){
   var joint_id = '#' + joint;
   var cur_deg = parseInt($(joint_id).text());
   servo(joint,cur_deg - 1);
+}
+
+function get_image(){
+    $.get(image_url, function(data){
+        $("#img_1").attr("src",data);
+        console.log("Retrieved Image");
+    });
 }
